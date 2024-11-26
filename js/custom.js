217f35c58b0991a76cc609af40fee2cce41c1f77
Rechel <ajw0822@mail.ustc.edu.cn>
2024-11-26 18:52:58 +0800

    Site updated: 2024-11-26 18:52:52

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,165 @@
-function catalogActive(){const e=document.getElementById("catalog-list");if(!e)return;e.addEventListener("wheel",(t=>{e.scrollLeft+=t.deltaY/2,t.preventDefault()}),!1);const t=decodeURIComponent(window.location.pathname).replace(/page\/[0-9]+\//g,""),n=document.getElementById(t);if(n){n.classList.add("selected");const t=n.offsetLeft-e.offsetLeft,l=(e.offsetWidth-n.offsetWidth)/2;e.scrollLeft=t-l}}catalogActive();var percentFlag=!1;function essayScroll(){const e=(document.documentElement.scrollTop||window.pageYOffset)%document.documentElement.clientHeight;result<=99||(result=99),!percentFlag&&e+100>=document.documentElement.clientHeight&&document.querySelector("#waterfall")?setTimeout((()=>{waterfall("#waterfall")}),500):setTimeout((()=>{document.querySelector("#waterfall")&&waterfall("#waterfall")}),500);const t=window.scrollY+document.documentElement.clientHeight;let n=document.getElementById("post-comment")||document.getElementById("footer");(n.offsetTop+n.offsetHeight/2<t||90<result)&&(percentFlag=!0)}function replaceAll(e,t,n){return e.split(t).join(n)}var temp={diffDate:function(e,t=!1){const n=new Date,l=new Date(e),a=n.getTime()-l.getTime(),o=36e5,c=24*o;let r;if(t){const e=a/c,t=a/o,n=a/6e4;r=a/(30*c)>=1?l.toLocaleDateString().replace(/\//g,"-"):e>=1?parseInt(e)+" "+GLOBAL_CONFIG.dateSuffix.day:t>=1?parseInt(t)+" "+GLOBAL_CONFIG.dateSuffix.hour:n>=1?parseInt(n)+" "+GLOBAL_CONFIG.dateSuffix.min:GLOBAL_CONFIG.dateSuffix.just}else r=parseInt(a/c);return r},changeTimeInEssay:function(){document.querySelector("#bber")&&document.querySelectorAll("#bber time").forEach((function(e){var t=e,n=t.getAttribute("datetime");t.innerText=temp.diffDate(n,!0),t.style.display="inline"}))},reflashEssayWaterFall:function(){document.querySelector("#waterfall")&&setTimeout((function(){waterfall("#waterfall"),document.getElementById("waterfall").classList.add("show")}),500)},commentText:function(e){"undefined"!=e&&"null"!=e||(e="好棒！");var t=document.getElementsByClassName("el-textarea__inner")[0],n=document.createEvent("HTMLEvents");if(t){n.initEvent("input",!0,!0);var l=replaceAll(e,"\n","\n> ");t.value="> "+l+"\n\n",t.dispatchEvent(n);var a=document.querySelector("#post-comment").offsetTop;window.scrollTo(0,a-80),t.focus(),t.setSelectionRange(-1,-1),document.getElementById("comment-tips")&&document.getElementById("comment-tips").classList.add("show")}}};function PublicSacrificeDay(){var e=new Array("0707","0918","1213"),t=new Date,n="",l=t.getMonth()+1;return t.getMonth()>9?n+=l:n+="0"+l,t.getDate()>9?n+=t.getDate():n+="0"+t.getDate(),e.indexOf(n)>-1?1:0}temp.changeTimeInEssay(),temp.reflashEssayWaterFall(),PublicSacrificeDay()&&document.getElementsByTagName("html")[0].setAttribute("style","filter:gray !important;filter:grayscale(100%);-webkit-filter:grayscale(100%);-moz-filter:grayscale(100%);-ms-filter:grayscale(100%);-o-filter:grayscale(100%);");
+function catalogActive() {
+  const CATALOG_LIST_ID = 'catalog-list';
+  const SCROLL_SPEED = 2; // 调整滚动速度的因子
+
+  const $list = document.getElementById(CATALOG_LIST_ID);
+  
+  if (!$list) return; // 如果找不到元素，则直接返回
+
+  // 滚动事件处理函数
+  const handleScroll = (e) => {
+      $list.scrollLeft += e.deltaY / SCROLL_SPEED; // 使用 deltaY 进行水平滚动
+      e.preventDefault(); // 阻止浏览器默认滚动行为
+  };
+
+  // 添加鼠标滚轮事件监听
+  $list.addEventListener('wheel', handleScroll, false);
+
+  // 高亮当前页面对应的分类或标签
+  const path = decodeURIComponent(window.location.pathname).replace(/page\/[0-9]+\//g, '');
+  const $catalog = document.getElementById(path);
+  
+  if ($catalog) {
+      $catalog.classList.add('selected'); // 高亮选中的分类或标签
+
+      // 滚动当前页面对应的分类或标签到中部
+      const catalogOffset = $catalog.offsetLeft - $list.offsetLeft;
+      const offsetAdjustment = ($list.offsetWidth - $catalog.offsetWidth) / 2;
+      $list.scrollLeft = catalogOffset - offsetAdjustment;
+  }
+}
+
+// 调用函数
+catalogActive();
+
+//即刻短文
+var percentFlag = false; // 节流阀
+function essayScroll() {
+  let a = document.documentElement.scrollTop || window.pageYOffset; // 卷去高度
+  const waterfallResult = a % document.documentElement.clientHeight; // 卷去一个视口
+  result <= 99 || (result = 99);
+
+  if (
+    !percentFlag &&
+    waterfallResult + 100 >= document.documentElement.clientHeight &&
+    document.querySelector("#waterfall")
+  ) {
+    // console.info(waterfallResult, document.documentElement.clientHeight);
+    setTimeout(() => {
+      waterfall("#waterfall");
+    }, 500);
+  } else {
+    setTimeout(() => {
+      document.querySelector("#waterfall") && waterfall("#waterfall");
+    }, 500);
+  }
+
+  const r = window.scrollY + document.documentElement.clientHeight;
+
+  let p = document.getElementById("post-comment") || document.getElementById("footer");
+
+  (p.offsetTop + p.offsetHeight / 2 < r || 90 < result) && (percentFlag = true);
+}
+
+function replaceAll(e, n, t) {
+  return e.split(n).join(t);
+}
+var temp = {
+  diffDate: function (d, more = false) {
+    const dateNow = new Date();
+    const datePost = new Date(d);
+    const dateDiff = dateNow.getTime() - datePost.getTime();
+    const minute = 1000 * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const month = day * 30;
+
+    let result;
+    if (more) {
+      const monthCount = dateDiff / month;
+      const dayCount = dateDiff / day;
+      const hourCount = dateDiff / hour;
+      const minuteCount = dateDiff / minute;
+
+      if (monthCount >= 1) {
+        result = datePost.toLocaleDateString().replace(/\//g, "-");
+      } else if (dayCount >= 1) {
+        result = parseInt(dayCount) + " " + GLOBAL_CONFIG.dateSuffix.day;
+      } else if (hourCount >= 1) {
+        result = parseInt(hourCount) + " " + GLOBAL_CONFIG.dateSuffix.hour;
+      } else if (minuteCount >= 1) {
+        result = parseInt(minuteCount) + " " + GLOBAL_CONFIG.dateSuffix.min;
+      } else {
+        result = GLOBAL_CONFIG.dateSuffix.just;
+      }
+    } else {
+      result = parseInt(dateDiff / day);
+    }
+    return result;
+  },
+  changeTimeInEssay: function () {
+    document.querySelector("#bber") &&
+      document.querySelectorAll("#bber time").forEach(function (e) {
+        var t = e,
+          datetime = t.getAttribute("datetime");
+        (t.innerText = temp.diffDate(datetime, true)), (t.style.display = "inline");
+      });
+  },
+  reflashEssayWaterFall: function () {
+    document.querySelector("#waterfall") &&
+      setTimeout(function () {
+        waterfall("#waterfall");
+        document.getElementById("waterfall").classList.add("show");
+      }, 500);
+  },
+  commentText: function (e) {
+    if (e == "undefined" || e == "null") e = "好棒！";
+    var n = document.getElementsByClassName("el-textarea__inner")[0],
+      t = document.createEvent("HTMLEvents");
+    if (!n) return;
+    t.initEvent("input", !0, !0);
+    var o = replaceAll(e, "\n", "\n> ");
+    (n.value = "> " + o + "\n\n"), n.dispatchEvent(t);
+    var i = document.querySelector("#post-comment").offsetTop;
+    window.scrollTo(0, i - 80),
+      n.focus(),
+      n.setSelectionRange(-1, -1),
+      document.getElementById("comment-tips") && document.getElementById("comment-tips").classList.add("show");
+  },
+};
+
+temp.changeTimeInEssay();
+temp.reflashEssayWaterFall();
+
+
+//祭奠日变灰
+if(PublicSacrificeDay()){
+  document.getElementsByTagName("html")[0].setAttribute("style","filter:gray !important;filter:grayscale(100%);-webkit-filter:grayscale(100%);-moz-filter:grayscale(100%);-ms-filter:grayscale(100%);-o-filter:grayscale(100%);");
+}
+
+function PublicSacrificeDay(){
+    var PSFarr=new Array("0707","0918","1213");
+    var currentdate = new Date();
+    var str = "";
+    var mm = currentdate.getMonth()+1;
+    if(currentdate.getMonth()>9){
+      str += mm;
+    }else{
+      str += "0" + mm;
+    }
+    if(currentdate.getDate()>9){
+      str += currentdate.getDate();
+    }else{
+      str += "0" + currentdate.getDate();
+    }
+    if(PSFarr.indexOf(str)>-1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+// 自动网站变灰
+// 0707 - 七七事变
+// 0918 - 九一八事变
+// 1213 - 南京公祭日
--- js/hot.js
+++ js/hot.js
@@ -1,1 +1,23 @@
-fetch("https://wb.bore.vip/api").then((t=>t.json())).then((t=>{let o="<style>.hot-new.hot-hot.hot-jyzy.hot-recommend.hot-adrecommend.hot-friend.hot-boom.hot-topic.hot-topic-ad.hot-boil#hot-container{overflow-y:auto;-ms-overflow-style:none;scrollbar-width:none}#hot-container::-webkit-scrollbar{display:none}.hot-list-item{display:flex;flex-direction:row;justify-content:space-between;flex-wrap:nowrap}.hot-title{white-space:nowrap;overflow:hidden;text-overflow:ellipsis;margin-right:auto}.hot-num{float:right}.hot-hotness{display:inline-block;padding:0 6px;transform:scale(.8) translateX(-3px);color:#fff;border-radius:8px}</style>";o+='<div class="hot-list">';let e={"爆":"hot-boom","热":"hot-hot","沸":"hot-boil","新":"hot-new","荐":"hot-recommend","音":"hot-jyzy","影":"hot-jyzy","剧":"hot-jyzy","综":"hot-jyzy"};for(let n of t)o+='<div class="hot-list-item"><div class="hot-hotness '+e[n.hot||"荐"]+'">'+(n.hot||"荐")+'</div><span class="hot-title"><a title="'+n.title+'"href="'+n.url+'" target="_blank" rel="external nofollow noreferrer">'+n.title+'</a></span><div class="hot-num"><span>'+n.num+"</span></div></div>";o+="</div>",document.getElementById("hot-container").innerHTML=o})).catch((function(t){console.log(t)}));
+fetch('https://wb.bore.vip/api').then(data=>data.json()).then(data=>{
+    let html = '<style>.hot-new.hot-hot.hot-jyzy.hot-recommend.hot-adrecommend.hot-friend.hot-boom.hot-topic.hot-topic-ad.hot-boil#hot-container{overflow-y:auto;-ms-overflow-style:none;scrollbar-width:none}#hot-container::-webkit-scrollbar{display:none}.hot-list-item{display:flex;flex-direction:row;justify-content:space-between;flex-wrap:nowrap}.hot-title{white-space:nowrap;overflow:hidden;text-overflow:ellipsis;margin-right:auto}.hot-num{float:right}.hot-hotness{display:inline-block;padding:0 6px;transform:scale(.8) translateX(-3px);color:#fff;border-radius:8px}</style>'
+    html += '<div class="hot-list">'
+    let hotness = {
+        '爆': 'hot-boom',
+        '热': 'hot-hot',
+        '沸': 'hot-boil',
+        '新': 'hot-new',
+        '荐': 'hot-recommend',
+        '音': 'hot-jyzy',
+        '影': 'hot-jyzy',
+        '剧': 'hot-jyzy',
+        '综': 'hot-jyzy'
+    }
+    for (let item of data) {
+        html += '<div class="hot-list-item"><div class="hot-hotness ' + hotness[(item.hot || '荐')] + '">' + (item.hot || '荐') + '</div>' + '<span class="hot-title"><a title="' + item.title + '"href="' + item.url + '" target="_blank" rel="external nofollow noreferrer">' + item.title + '</a></span>' + '<div class="hot-num"><span>' + item.num + '</span></div></div>'
+    }
+    html += '</div>'
+    document.getElementById('hot-container').innerHTML = html
+}
+).catch(function(error) {
+    console.log(error);
+});
